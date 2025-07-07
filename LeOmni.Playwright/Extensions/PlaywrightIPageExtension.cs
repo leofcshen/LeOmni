b9fd72509334b830222b4b144c89bc92a499a1aa
rovingwind <rovingwind93@gmail.com>
2025-07-08 01:20:57 +0800

    update

---
+++ LeOmni.Playwright/Extensions/PlaywrightIPageExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace LeOmni.Playwright.Extensions;
+
+public static class PlaywrightIPageExtension {
+  public static async Task<string> GetXPathAsync(this IPage page, string xpath) {
+    var locator = page.Locator($"xpath={xpath}");
+    await locator.WaitForAsync();
+
+    return await locator.InnerTextAsync();
+  }
+
+  public static async Task<List<string>> GetXPathAsync(this IPage page, params string[] xpaths) {
+    var results = new List<string>();
+
+    foreach (var xpath in xpaths) {
+      var locator = page.Locator($"xpath={xpath}");
+      await locator.WaitForAsync();
+      results.Add(await locator.InnerTextAsync());
+    }
+
+    return results;
+  }
+}
---
+++ LeOmni.Playwright/Services/PlaywrightService.cs
@@ -0,0 +1,30 @@
+﻿namespace LeOmni.Playwright.Services;
+
+public class PlaywrightService : IAsyncDisposable {
+  private readonly IPlaywright _playwright;
+  private readonly IBrowser _browser;
+  public IPage Page { get; private set; }
+
+  private PlaywrightService(IPlaywright playwright, IBrowser browser, IPage page) {
+    _playwright = playwright;
+    _browser = browser;
+    Page = page;
+  }
+
+  public static async Task<PlaywrightService> CreateAsync(bool headless = false) {
+    var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+    var browser = await playwright.Chromium.LaunchAsync(new() {
+      Headless = headless
+    });
+    var page = await browser.NewPageAsync();
+
+    return new PlaywrightService(playwright, browser, page);
+  }
+
+  public async ValueTask DisposeAsync() {
+    await Page.CloseAsync();
+    await _browser.CloseAsync();
+    _playwright.Dispose();
+    GC.SuppressFinalize(this);
+  }
+}
