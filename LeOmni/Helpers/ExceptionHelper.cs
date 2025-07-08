0cec1bddfa695fd275f39b77393b2f2830ba7e41
Leo Shen <rovingwind93@gmail.com>
2025-07-08 14:15:47 +0800

    update

---
+++ LeOmni/Helpers/ExceptionHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace LeOmni.Helpers;
+
+public sealed class ExceptionHelper {
+  public static async Task<string> CatchAsync(Func<Task<string>> func) {
+    try {
+      return await func();
+    } catch (Exception ex) {
+      return $"出錯: {ex.Message}";
+    }
+  }
+}
